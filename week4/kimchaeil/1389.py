9f33bd4a7fe20d3a3de09f5bbc88daa2921b8b3e
LES8638 <rlacodlf11@naver.com>
2024-04-03 15:12:46 +0900

    week4: 김채일(3/3)

---
+++ week4/kimchaeil/1389.py
@@ -0,0 +1,22 @@
+import sys
+
+input = sys.stdin.readline
+
+N, M = map(int, input().split())
+
+mt = [[N for _ in range(N)] for _ in range(N)]
+
+for _ in range(M):
+    i, j = map(int, input().split())
+    mt[i-1][j-1] = 1; mt[j-1][i-1]=1
+
+for via in range(N):
+    for start in range(N):
+        for end in range(N):
+            mt[start][end] = min(mt[start][end], mt[start][via]+mt[via][end])
+
+result = []
+for _ in range(N):
+    result.append(sum(mt[_]))
+
+print(result.index(min(result))+1)
