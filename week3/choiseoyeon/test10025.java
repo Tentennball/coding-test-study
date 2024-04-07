6693b21cc1b5d1240525ca59160757cd484771ad
SyeonC <108733727+SyeonC@users.noreply.github.com>
2024-04-07 20:54:46 +0900

    Update test10025.java

--- week3/choiseoyeon/test10025.java
+++ week3/choiseoyeon/test10025.java
@@ -12,33 +12,29 @@ public class test10025 {
 
 		int N = Integer.parseInt(st.nextToken());
 		int K = Integer.parseInt(st.nextToken());
+		int[] arr = new int[1000001];
 		
-		int arr[][] = new int[N][2];
-		int max_x = 0;
-		for(int i = 0; i < N; i++) {
+		for(int i=0; i<N; i++) {
 			st = new StringTokenizer(br.readLine());
 			int g = Integer.parseInt(st.nextToken());
 			int x = Integer.parseInt(st.nextToken());
-			
-			arr[i][0] = g;
-			arr[i][1] = x;
-			if(x > max_x)
-				max_x = x;
-			/*System.out.println(arr[i][0]);
-			System.out.println(arr[i][1]);*/
+			arr[x] += g;
 		}
 		
 		int sum = 0;
-		for(int i = 0; i < max_x; i++) {
-			if(arr[i][1] - K)
-		}
 		
-		// arr[i][1] - K, arr[i][1] + K
-		
-		int max = 0;
-		
-		
-		
+        for (int i = 0; i < 2*K+1 && i <= 1000000; i++) {
+            sum += arr[i];
+        }
+        
+        int max = sum;
+        for (int i = 2*K+1, j = 0; i <= 1000000; i++, j++) {
+            sum -= arr[j];
+            sum += arr[i];
+            if (sum > max)
+                max = sum;
+        }
+        System.out.println(max);
 	}
 
 }
