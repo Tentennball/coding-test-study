ea99df433c61a50b75980bbe1a8f0f15868be227
tentenball <lth8905@gmail.com>
2024-04-18 22:31:48 +0900

    week5 이태훈(3/3)

--- week5/taehoon/5639.py
+++ week5/taehoon/5639.py
@@ -1,12 +1,22 @@
+import sys
+sys.setrecursionlimit(10005)
 values = []
-
+si = sys.stdin.readline
 def travel(l, r):
     if(l>r): return
+    mid = r
+    for i in range(l+1, r+1):
+        if(values[i]>values[l]):
+            mid = i-1
+            break
+    travel(l+1, mid)
+    travel(mid+1, r)
+    print(values[l])
 
 while(True):
-    si = input()
-    if not si:
+    line = si()
+    if not line:
         break
-    values.append(int(si))
+    values.append(int(line))
 
-travel(0, len(values-1))
+travel(0, len(values)-1)
